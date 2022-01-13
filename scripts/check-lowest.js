faa9cac9967d66b5042b99b91b61d45747a5b3ef
Muhammad Adam <adam184688@gmail.com>
2022-01-14 03:55:10 +0500

    Button Logic Added

--- scripts/check-lowest.js
+++ scripts/check-lowest.js
@@ -2,6 +2,30 @@ console.log("Running Script");
 
 
 (async () => {
+    let CLICKED = false;
+    let refreshInterval;
+    let valueCheck;
+
+    const startWorking = () => {
+        refreshInterval = setInterval(() => {
+            document.querySelector('div[data-original-title^="Refresh"]').firstChild.click();
+        }, 2000)
+
+        valueCheck = setInterval(() => {
+            const priceNode = document.querySelector('div[class="my-2 card__price tw-truncate"]')?.innerText
+            const price = Number(priceNode.split(' ')[0]);
+            const thresholdValue = Number(document.querySelector('input#threshold').value)
+            if (price < thresholdValue) {
+                console.log("BUY IT");
+            }
+        }, 1000)
+    }
+
+    const stopWorking = () => {
+       clearInterval(refreshInterval);
+       clearInterval(valueCheck);
+    }
+
 
     const interval = setInterval(() => {
         
@@ -12,12 +36,29 @@ console.log("Running Script");
         mainDiv.className = 'mainDiv';
         const a = document.createElement('input');
         const b = document.createElement('button');
-        b.className = 'startButton'
+        const c = document.createElement('button');
+        b.className = 'startButton';
+        c.className = 'startButton';
         a.className = 'test';
         a.placeholder = 'Enter Threshold Value';
+        a.id = 'threshold';
 
-        b.innerText = 'START';
-        mainDiv.append(b, a);
+        b.innerText = 'Start';
+        c.innerText = 'Stop';
+        b.addEventListener('click', () => {
+            if (!CLICKED) {
+                startWorking();
+                CLICKED = true;
+            }
+        })
+
+        c.addEventListener('click', () => {
+            if (CLICKED) {
+                CLICKED = false;
+                stopWorking();
+            }
+        })
+        mainDiv.append(b,c, a);
     
         collectionDiv?.insertBefore(mainDiv, collectionDiv.firstChild);
     
